cc6ead7e36a6c7bf0dad6cb313dafd048f238989
laotianwy <1695657342@qq.com>
2025-01-20 02:45:11 +0800

    启用全局前缀和跨域

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-19 20:42:21
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-20 01:11:35
+ * @LastEditTime: 2025-01-20 02:44:45
  * @FilePath: /mock-api-serve/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,6 +12,14 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+    // 允许跨域
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    });
+    // 启用全局前缀
+    app.setGlobalPrefix('mock');
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
