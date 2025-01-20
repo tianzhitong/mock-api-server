a7dd66aa68ad515e48c1bf0c213fa22033a83d1f
laotianwy <1695657342@qq.com>
2025-01-21 03:07:17 +0800

    fix：使用count查询添加where语句

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-19 21:06:00
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-21 02:59:48
+ * @LastEditTime: 2025-01-21 03:06:36
  * @FilePath: /mock-api-serve/src/user/user.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -41,7 +41,7 @@ export class UserService {
                 skip,
                 take,
             });
-            const total = await tx.user.count();
+            const total = await tx.user.count({ where });
 
             return {
                 data: findManyUserList,
