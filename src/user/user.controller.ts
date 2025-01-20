a6f0eb9fb46fd92c44c9750c27fd9448132ec3fb
laotianwy <1695657342@qq.com>
2025-01-21 01:38:54 +0800

    feat：返回image swagger定义修改

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-19 21:06:00
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-21 01:24:00
+ * @LastEditTime: 2025-01-21 01:32:26
  * @FilePath: /mock-api-serve/src/user/user.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -72,6 +72,15 @@ export class UserController {
     // @ApiResult({ model: String })
     @ApiResponse({
         status: 200,
+        description: '获取验证码',
+        content: {
+            'image/svg+xml': {
+                schema: {
+                    type: 'string',
+                    format: 'binary',
+                },
+            },
+        },
     })
     async getCaptcha(@Res() res: Response) {
         // 生成验证码svg数据
