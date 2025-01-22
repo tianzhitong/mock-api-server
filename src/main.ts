fff979bb0232cad701f59a4bd040740007fdeeb0
laotianwy <1695657342@qq.com>
2025-01-23 00:17:52 +0800

    feat：根据yml配置判断是否启用swagger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-19 20:42:21
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-20 23:53:47
+ * @LastEditTime: 2025-01-23 00:13:26
  * @FilePath: /mock-api-serve/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,13 +10,12 @@ import { NestFactory } from '@nestjs/core';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { writeFileSync } from 'fs';
-import { join } from 'path';
+
 import * as fastifyRateLimit from '@fastify/rate-limit';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { setupSwagger } from './setup-swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
@@ -32,6 +31,8 @@ async function bootstrap() {
     app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
     // 读取环境配置
     const envConfig = app.get(ConfigService);
+
+    // 获取端口号
     const PORT = envConfig.get<number>('app.port') ?? 8080;
 
     // 启用接口限制器
@@ -43,28 +44,8 @@ async function bootstrap() {
     // 启用全局接口前缀
     app.setGlobalPrefix('mock');
 
-    // Swagger 配置
-    const config = new DocumentBuilder()
-        .setTitle('Mock API') // 标题
-        .setDescription('The Mock API description') // 描述
-        .setVersion('1.0') // api版本
-        .addTag('MOCK') // 标签
-        .addCookieAuth('connect.sid') // 添加 Cookie 认证方式，名称为 'connect.sid'
-        .addBearerAuth({
-            // 添加 Bearer 认证方式，类型为 'http'，认证方案为 'bearer'
-            type: 'http',
-            scheme: 'bearer',
-        })
-        .build();
-
-    // 使用配置对象创建 Swagger 文档
-    const document = SwaggerModule.createDocument(app, config);
-    // 生成的openai数据写入到本地项目
-    writeFileSync(join(process.cwd(), 'openApi.json'), JSON.stringify(document, null, 2));
-    // 设置 Swagger 模块的路径和文档对象，将 Swagger UI 绑定到 '/api-doc' 路径上
-    SwaggerModule.setup('api-doc', app, document, {
-        jsonDocumentUrl: 'swagger/json',
-    });
+    // 根据环境判断是否启用swagger文档
+    setupSwagger(app, envConfig);
 
     await app.listen(PORT);
 }
---
+++ src/setup-swagger.ts
@@ -0,0 +1,48 @@
+/*
+ * @Author: laotianwy 1695657342@qq.com
+ * @Date: 2025-01-23 00:05:38
+ * @LastEditors: laotianwy 1695657342@qq.com
+ * @LastEditTime: 2025-01-23 00:16:23
+ * @FilePath: /mock-api-serve/src/setup-swagger.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+export const setupSwagger = (app: INestApplication, configService: ConfigService) => {
+    const enableSwagger = configService.get<boolean>('swagger.enabled');
+    const swaggerPath = configService.get<string>('swagger.path');
+
+    if (!enableSwagger) return;
+
+    // Swagger 配置
+    const config = new DocumentBuilder()
+        .setTitle('Mock API') // 标题
+        .setDescription('The Mock API description') // 描述
+        .setVersion('1.0') // api版本
+        .addTag('MOCK') // 标签
+        .addCookieAuth('connect.sid') // 添加 Cookie 认证方式，名称为 'connect.sid'
+        .addBearerAuth({
+            // 添加 Bearer 认证方式，类型为 'http'，认证方案为 'bearer'
+            type: 'http',
+            scheme: 'bearer',
+        })
+        .build();
+
+    // 使用配置对象创建 Swagger 文档
+    const document = SwaggerModule.createDocument(app, config);
+    // 生成的openai数据写入到本地项目
+    writeFileSync(join(process.cwd(), 'openApi.json'), JSON.stringify(document, null, 2));
+    // 设置 Swagger 模块的路径和文档对象，将 Swagger UI 绑定到 '/api-doc' 路径上
+    SwaggerModule.setup(swaggerPath, app, document, {
+        jsonDocumentUrl: 'swagger/json',
+    });
+
+    console.log(
+        `Swagger UI available at ----> http://localhost:${configService.get<number>('app.port')}/${swaggerPath}`,
+    );
+};
