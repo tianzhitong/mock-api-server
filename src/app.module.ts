8aad000884fc564f0c3c14834aee50e22a3f24b3
laotianwy <1695657342@qq.com>
2025-01-23 18:46:39 +0800

    feat：接口控制器目录修改

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,9 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { MockModule } from './mock/mock.module';
-import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { MockModule } from './modules/mock/mock.module';
+import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ApiInterceptorInterceptor } from './api-interceptor/api-interceptor.interceptor';
 import { ConfigModule } from '@nestjs/config';
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from 'src/modules/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { BcryptService } from 'src/utils/services/bcrypt/bcrypt.service';
 import { $Enums } from '@prisma/client';
