de4ee181a27c8ea3f10a136c7f7c4446a0a92854
laotianwy <1695657342@qq.com>
2025-02-10 20:09:38 +0800

    feat：节流次数修改

--- restart_service.sh
+++ restart_service.sh
@@ -9,11 +9,11 @@ echo "拉取最新服务..."
 docker compose pull $SERVICE_NAME
 
 # 检查容器是否正在运行
-if [ $(docker ps -q -f name=$CONTAINER_NAME) ]; then
-    echo "停止并删除现有的容器..."
-    docker stop $CONTAINER_NAME
-    docker rm $CONTAINER_NAME
-fi
+# if [ $(docker ps -q -f name=$CONTAINER_NAME) ]; then
+#     echo "停止并删除现有的容器..."
+#     docker stop $CONTAINER_NAME
+#     docker rm $CONTAINER_NAME
+# fi
 
 # 启动新容器
 echo "启动新容器..."
