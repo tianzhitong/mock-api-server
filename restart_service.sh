73b87d5ac718edf417fb6fa0ccddf366808a8cfe
laotianwy <1695657342@qq.com>
2025-02-11 02:40:02 +0800

    docs：部署文档修改

--- restart_service.sh
+++ restart_service.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-# 设置变量
-CONTAINER_NAME="nestjs_app"
-SERVICE_NAME="app"
-
 # 拉取最新镜像
-echo "拉取最新服务..."
-docker compose pull $SERVICE_NAME
-
-# 检查容器是否正在运行
-# if [ $(docker ps -q -f name=$CONTAINER_NAME) ]; then
-#     echo "停止并删除现有的容器..."
-#     docker stop $CONTAINER_NAME
-#     docker rm $CONTAINER_NAME
-# fi
-
-# 启动新容器
-echo "启动新容器..."
-docker compose up -d --no-deps $MICRO_NAME
+docker service update \
+  --image tianzhitong/mock-app-server:latest \
+  mock-server-stack_app
 
 echo "服务已重启。"
