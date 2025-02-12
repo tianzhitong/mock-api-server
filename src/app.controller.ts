69638643cfd0d3cdb946b54f3e52dcb7b11066f8
laotianwy <1695657342@qq.com>
2025-02-12 20:04:52 +0800

    feat：test build

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-26 22:11:41
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-02-12 14:35:51
+ * @LastEditTime: 2025-02-12 20:04:41
  * @FilePath: /mock-api-serve/src/app.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,6 +14,6 @@ import { ApiTags } from '@nestjs/swagger';
 export class AppController {
     @Get('hello')
     checkNetwork() {
-        return 'hello nestjs!';
+        return 'hello nestjs! next build';
     }
 }
