56590e9642de5e1b6ba68a760038908bc9cfabd0
laotianwy <1695657342@qq.com>
2025-02-12 20:26:44 +0800

    build：cicd修改

--- restart_service.sh
+++ restart_service.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+# 检查是否传递了镜像标签参数
+if [ -z "$1" ]; then
+  echo "错误：未提供镜像标签参数。"
+  exit 1
+fi
 
-# 拉取最新镜像
+IMAGE_TAG=$1
+SERVICE_NAME="mock-server-stack_app"
+
+# 更新服务镜像
+echo "正在更新服务 $SERVICE_NAME 的镜像为 $IMAGE_TAG..."
 docker service update \
-  --image tianzhitong/mock-app-server:latest \
-  mock-server-stack_app
+  --image $IMAGE_TAG \
+  $SERVICE_NAME
 
 echo "服务已重启。"
