3dd534b0e2895514de2c02cb72c69a2e88006a44
laotianwy <1695657342@qq.com>
2025-02-16 14:55:06 +0800

    fix：修复客户端只能使用get获取mock数据的问题

--- src/modules/mock/mock.controller.ts
+++ src/modules/mock/mock.controller.ts
@@ -2,11 +2,11 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-19 21:05:20
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-01-24 19:00:05
+ * @LastEditTime: 2025-02-16 14:53:49
  * @FilePath: /mock-api-serve/src/mock/mock.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Post, All, Query } from '@nestjs/common';
 import { MockService } from './mock.service';
 import { ApiTags } from '@nestjs/swagger';
 import { ClientGetMockDataDtO, CreateMockListDto } from './dto/create-mock.dto';
@@ -22,8 +22,8 @@ export class MockController {
         await this.mockService.createMockApi(createMockListDto.list);
     }
 
-    @Get('clientGetMockData')
     @Bypass()
+    @All('clientGetMockData')
     async clientGetMockData(@Query() clientGetMockDataDtO: ClientGetMockDataDtO) {
         return await this.mockService.clientGetMockData(clientGetMockDataDtO);
     }
