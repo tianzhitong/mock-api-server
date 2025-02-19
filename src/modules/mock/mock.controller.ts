8d7f297dc32dfd68acdd30ace52c23c514b75ca5
laotianwy <1695657342@qq.com>
2025-02-19 20:00:42 +0800

    feat：删除项目添加权限，只有管理员才可以删除项目

--- src/modules/mock/mock.controller.ts
+++ src/modules/mock/mock.controller.ts
@@ -2,7 +2,7 @@
  * @Author: laotianwy 1695657342@qq.com
  * @Date: 2025-01-19 21:05:20
  * @LastEditors: laotianwy 1695657342@qq.com
- * @LastEditTime: 2025-02-19 19:54:22
+ * @LastEditTime: 2025-02-19 19:56:45
  * @FilePath: /mock-api-serve/src/mock/mock.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,6 +13,8 @@ import { ClientGetMockDataDtO, CreateMockListDto } from './dto/create-mock.dto';
 import { Bypass } from 'src/common/decorators/bypass.decorator';
 import { DeleteMockDto } from './dto/delete-mock.dto';
 import { ApiResult } from 'src/common/decorators/api-result.decorator';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from '@prisma/client';
 
 @ApiTags('Mock')
 @Controller('mock')
@@ -20,6 +22,7 @@ export class MockController {
     constructor(private readonly mockService: MockService) {}
 
     @Post('delelteMockByProjectId')
+    @Roles(Role.ADMIN)
     @ApiResult({
         model: Boolean,
     })
